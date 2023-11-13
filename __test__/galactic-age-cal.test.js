1895b06bbba4043ab988daf1a0092e26c70eeaa3
ZuriGa <zurisadailopezmartinez@gmail.com>
2023-11-12 20:08:24 -0800

    first test practice

--- __test__/galactic-age-cal.test.js
+++ __test__/galactic-age-cal.test.js
@@ -1,4 +1,7 @@
-import GalacticAge from './../src/galactic-age-cal.js';
-class SpaceAge {
-    
-}
+import { GalacticAge } from './../src/galactic-age-cal.js';
+
+describe(GalacticAge, () => {
+    test('It should correctly calculate a users age from a users input date: 11/14/1990', () => {
+        expect()
+    })
+})
